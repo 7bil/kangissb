eec1d347ca52cf2dc490b00fa3d23815dba29a01
7bil <96629881+7bil@users.noreply.github.com>
2022-01-09 17:07:29 +0800

    Update loaded_callback.lua

--- loaded_callback.lua
+++ loaded_callback.lua
@@ -1,13 +1,13 @@
-local G1O = {}
+local Kang = {}
 
 -- Cache func to var
 local setmetatable, entity_hitbox_position, client_visible, entity_get_players, entity_get_prop, globals_tickinterval = setmetatable, entity.hitbox_position, client.visible, entity.get_players, entity.get_prop, globals.tickinterval
 --Private function end
-function G1O.why_crack_me()
-    print'LOADED GIO - Q:397216779'
+function Kang.why_crack_me()
+    print'LOADED - Q:397216779'
 end
 
-return setmetatable(G1O,{
+return setmetatable(Kang,{
     __call = function(...)
         return  G1O.why_crack_me(...)
     end
